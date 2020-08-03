48e28cf891490c85e08f41d11a360560f6ab1645
Richard Kelly <rk@codeden.dev>
2020-08-03 07:52:41 +0100

    initialized project
    Project Start:
    
    Established Architecture structure using MVVM pattern.
    
    Next:
    
    Consume API, set up models.

---
+++ Forcast/Forcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forcast
+//
+//  Created by Richard Kelly on 03/08/2020.
+//  Copyright © 2020 Richard Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
